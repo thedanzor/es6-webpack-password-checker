39c16854c59e4aa2a8c09f2f4af413a1c2273130
Scott Jones <scott@crowdynews.com>
2017-09-15 14:40:11 +0200

    Implement unit tests for state utility

---
+++ test/utils-state.js
@@ -0,0 +1,15 @@
+import test from 'ava';
+import { init, getState } from '../src/utils/setupState';
+
+test('State Utility - Setup and validate a basic state', t => {
+  init({
+    name: 'scott',
+    lastname: 'jones',
+    email: 'check github'
+  });
+
+  const { name, lastname, email } = getState();
+  t.is(name, 'scott');
+  t.is(lastname, 'jones');
+  t.is(email, 'check github');
+});
