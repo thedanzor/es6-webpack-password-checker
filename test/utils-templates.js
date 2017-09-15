c8fedd441ffea9590c0122e592def9512fc309bf
Scott Jones <scott@crowdynews.com>
2017-09-15 22:41:12 +0200

    Include test for the status view template

--- test/utils-templates.js
+++ test/utils-templates.js
@@ -5,14 +5,33 @@ import templates from '../src/views'; // Templates
 test('Template Utility - Setup and validate basic templates', t => {
   viewInit(templates);
 
-  const template = viewTemplate('defaultView');
-  const view = template('en');
+  const defaultTemplate = viewTemplate('defaultView');
+  const defaultView = defaultTemplate('en');
+
+  // Show the status template with a completed template
+  const statusTemplate = viewTemplate('status');
+  const statusView = statusTemplate('en', 5);
 
   t.is(
-    view,
+    defaultView,
     `
 			<div class="password-message-wrapper">
 				Please enter a secure password
+			</div>
+		`
+  );
+  t.is(
+    statusView,
+    `
+			<div
+				class="password-status-wrapper"
+				data-status="5"
+				data-tooltip="You should aim to be in the green">
+				<div class="status-one"></div>
+				<div class="status-two"></div>
+				<div class="status-three"></div>
+				<div class="status-four"></div>
+				<div class="status-five"></div>
 			</div>
 		`
   );
